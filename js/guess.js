d72e6fba495c030bef5408b7b588a8590882d03a
Paul Goodman <paulsaywardgoodman@gmail.com>
2015-02-25 14:31:42 -0500

    Finish guessing game.

--- js/guess.js
+++ js/guess.js
@@ -1,18 +1,22 @@
-// YOUR CODE GOES HERE
 var randNumber = Math.floor(Math.random() * 100 + 1 );
 
 function testGuess(guess, answer) {
-  if (guess == answer) {
+  if (isNaN(guess)) {
+    var clue = "You must guess a number. Try again!"
+  }
+    else if (guess == answer) {
     var clue = guess + " is correct!";
   } else if (guess < answer) {
     var clue = guess + " is too low.";
   } else {
     var clue = guess + " is too high.";
   }
+  return clue;
 }
 
 $(".guess_number").on("submit", function(event) {
-  var guess = parseInt($("#guess"));
-}
-
-console.log(randNumber);
+  var guess = parseInt($("#guess").val());
+  // debugger;
+  $("ul").append("<li>" + testGuess(guess, randNumber) + "</li>");
+  return false;
+})
